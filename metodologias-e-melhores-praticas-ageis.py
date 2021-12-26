5985883aa15aaf529f2f2cc445443af530e13c1c
Henrique Daniel <henriquec.daniel@hotmail.com>
2021-12-26 20:49:25 -0300

    metodologias-e-melhores-praticas-ageis.py

---
+++ metodologias-e-melhores-praticas-ageis.py
@@ -0,0 +1,234 @@
+'''
+metodologia de desenvolvimento de software é um conjunto de métodos coordenados para se alcançar  um  objetivo,
+através  de  uma  dinâmica  iterativa  visando à qualidade  e à produtividade  dos  projetos.
+
+TPS (toyota production system ou sistema de produção toyota) x Waterfall
+
+
+Waterfall
+sequência de fases no desenvolvimento de softwareque são: análise e definição dos requisitos, design, desenvolvimento,
+testes e manutenção
+
+Escopo  engessado  no  começo  do  projeto
+Desperdício   com   funcionalidades   desnecessárias
+Entregas   lentas
+Baixa  transparência
+
+
+Manifesto Ágil (valores)
+Indivíduos   e   iteraçõesmais   que   processos   e   ferramentas
+Software  em  funcionamentomais  que  documentação  abrangente
+Colaboração  com  o  clientemais  que  negociação  de  contratos
+Responder a mudançasmais que seguir um plano
+
+
+Framework Scrum
+O Scrumé um framework simples para gerenciar projetos complexos, através de  uma  dinâmica,  papéis  e  cerimônias  no
+decorrer  de  todo o ciclo
+O Scrumnão restringe sua aplicação apenas para aárea de TI, ele pode ser aplicado em projetos diversos que possuem
+complexidade.A abordagem incremental e  iterativa  é  utilizada  fortemente  no Scrum,  onde  a  cada  determinado
+espaço  de tempo, uma funcionalidade do produto que faz parte de umtodo é entregue
+
+Pilares
+Transparência: Todos possuem o conhecimento dos requisitos, processos e do andamento do projeto
+
+Inspeção: durante  todo  o  tempo  é  inspecionado  o  que  está  sendo  feito, através de reuniões diárias ou no
+momento de revisão
+
+Adaptação: Conforme  as  mudanças  vão  acontecendo,  tanto  o  processo quanto o  produto  podem  sofrer
+adaptações,  desde  que  preservadosos valores e práticas ágeis
+
+Papéis Fundamentais
+
+Product Owner (PO):Como o nome já diz, é o dono do produto, o único responsável por decidir quais os recursos e
+funcionalidades o produto deve ter  e  qual  a  ordem  de  prioridade  que  deve  ser  desenvolvido Também mantém e
+comunica uma visão clara do que o Time Scrumestá trabalhando no produto, sendo um ponto de intersecção entre a área de
+negócio e a área de desenvolvimento
+
+Scrum  Master (SM):Responsável por ajudar os envolvidos a entender a cultura  ágil  com  todos  os  seus  valores  e
+princípios,  além  de  todas  as práticas do framework Scrum. Ele atua como um líder do processo e ajuda os  demais a
+desenvolver  sua  própria  forma  de  trabalho  com Scrum.  O Scrum  Master  também  é  um  facilitador,  contribuindo
+na  resolução  de conflitos   e   remoção   de   impedimentos,   protegendo   o   time   contra interferências externas
+que possam acontecer
+
+Time Scrum:Time  de  desenvolvimento  que  possui  vários  membros  com diferentes  funções,  que  possuem  as
+habilidades necessárias  para  o desenvolvimento do software, é o Time Scrum que decide como fazer para se  alcançar
+o  objetivo  do  desenvolvimento.  Normalmente  o  time  Scrum possui entre 4 e 8 pessoas, em que os mesmos colaboram
+entre si para se tornarem   um   time   auto-organizado   e   multidisciplinar.   Quando   são necessárias  equipes
+maiores  para  o  desenvolvimento  de  um  projeto, em vez de montar uma única equipe Scrumcom,por exemplo,50 pessoas,
+o ideal seria montar vários times Scrum menores.
+
+Valores do Scrum
+Foco: Todos  estão  focados  em  um  único  objetivo, em  vezde  cada  um trabalhar em diversas partes isoladas para
+entregar tudo junto no final. O time deve manter o foco em pequenas partes por vez, essas partes devem estar  alinhadas
+com  a  priorização  do Product  Owner, em  queo  mesmo estará  focado  nas  necessidades  mais  prioritárias  do  seu
+produto.  Para manter  o  foco  entre  ambas  as  partes,  o Scrum  Masterfacilita  o  meio  de campo e ajuda na
+remoção dos impedimentos
+
+Coragem: O  time  precisa  ter  coragem  para  enfrentar  os  desafios  mais diversos,independentementeda dificuldade,
+e sempre fazer as coisas do modo mais correto possível.No decorrer do tempo, imprevistos acontecerão e a coragem
+envolve aceitar as mudanças, errar, corrigir o erro e aprender com o mesmo o mais rápido possível.
+
+Comprometimento:Todos  os  papéis  possuem  seu  comprometimento,  o Time  Scrum  em  realizar  o  que  foi  acordado,
+o Scrum  Masterem “girar a roda” do framework Scrumjunto ao time, e o Product Owner em priorizar as necessidadesde
+negócio e tocar o produto.
+
+Respeito:Os  membros  dos  times  respeitam  um  ao  outro  e  se  sentem confortáveis  para  interagir,  ouvir  e
+respeitar  as  opiniões,  reconhecendo  e entendendo  as  diferenças  entre  as  diversas  personalidades  dentro
+do grupo, promovendo assim um ambiente saudável para o trabalho. O Product Ownerrespeita as decisões técnicas do
+Time Scrum, e em contrapartidao Time Scrum respeita as decisões de negócio vindas pelo Product Owner
+
+Abertura:Todos do time estão abertos aos trabalhos e desafios que estão por  vir,  também envolve  a  abertura  de
+oferecer  e  receber  feedbacks, dialogar  sobre  situações  e  criar  visibilidade  dos  problemasacontecidos, além da
+clareza do trabalho realizado e da performance do mesmo
+
+Princípios
+Controle dos processos empíricos
+Auto-organização
+Colaboração
+Priorização  baseada  em  valor
+time-boxing
+Desenvolvimento  iterativo
+
+Dinâmicas, cerimônias e artefatos
+No Scrum tudo começa com a visão de um produto, em queo Product Ownerdescreve o que ele quer do produto e aonde quer
+chegar, e,dada a visão macro, o Product Ownerlista as funcionalidades necessárias, essa lista é o artefato
+Product Backlog.
+Com o Backlogem mãos, o Product Ownerrealiza a priorização das atividades, seguindo  o  princípio  da  priorização
+baseada  em  valor.Cada  funcionalidade  do Backlogé definida como um artefato de User Stories(história de usuário),
+que possui como objetivo explicar a funcionalidade de forma simples, deixando claroquem é o usuário, o que ele quer e
+para qual objetivo.
+Para a execução dos User Storiesque estão no Backlog, no Scrum utilizamos o princípio de desenvolvimento iterativo
+através da Sprint, período de tempo limitado (time-boxing) de duração fixa (normalmente de 2 a 4 semanas) que cria algo
+de valor tangível para o cliente ou usuário do produto
+Antes de cada Sprintcomeçar é realizada uma cerimôniachamada de Sprint Planning,  para  determinar  qual  o  objetivo
+da  Sprint  e  quais User  Storiesserão desenvolvidas  naquela  Sprint.  Durante  o  planejamento,  o Product  Owner,
+Scrum Mastere Time Scrum precisam entrar em um consenso referente ao que cabe dentro daSprintbaseando-se na capacidade e
+velocidade da equipe. Caso existamdúvidas referentesaostamanhos dasatividades,utilizamos o Planning Pokerpara auxiliar
+na decisão,na qual,dado o escopo, os integrantes realizam votações com cartas para expor sua opinião referente ao
+tamanho daquela User Story.
+Com o Sprint definido, o desenvolvimento das atividades é iniciado, e todos os dias é realizadauma reunião de
+alinhamento rápido, essa cerimônia é chamada de Daily Scrum.
+Esse acompanhamento diário promove a transparência e o alinhamento constante entre o time  e a área  de  negócio, todos
+conseguem  visualizar  como  está  o  andamento  do Sprint com relação ao objetivo acordado
+Durante o período do Sprint, para saber se a atividade pode ser considerada concluída,utilizamos o conceito de
+Definition of Done(DoD), que é um acordo formal do timeque define claramente quais são os passos mínimos para a
+conclusão de um item potencialmente entregável.
+Perto de finalizar o Sprint, é realizada uma cerimônia para apresentar o que foi feito, o Sprint Review, com o intuito
+de verificar e adaptar o produto que está sendo desenvolvido. Por ser uma cerimônia mais para o final do Sprint, o ideal
+é que seja apresentado,durante o Sprint,o que está sendo desenvolvido ao Product Ownerde forma mais informal, para se
+necessário realizar alguma adaptação se possível dentro do próprio Sprint.Para  finalizar,  a  cerimônia  de
+Sprint  Retrospectiveé  realizada,  enquanto  o objetivo  do Sprint  Reviewé  verificar  necessidades  de  adaptaçõese
+melhoriasno produto,   o Sprint   Retrospectivetem   como   objetivo   verificar   necessidades   de adaptações e
+melhoria no processo de trabalhocomo um todo.É importante que na reunião de retrospectiva sejam levantados os pontos
+positivose a melhora (sempre focando nas causas raiz) da Sprint que passou
+O framework Scrum é recomendado para os times que querem iniciar na cultura ágile para projetos que têmuma janela de
+delivery constante, pois o mesmo possui uma  metodologia  bem  estruturadapontaapontae  promove  um  direcionamento
+do que deve ser feito, e,apesar disso, com o amadurecimento do time, o Scrum permite ser adaptado para melhor atender
+à necessidade
+
+
+
+Método Kanban
+Antes de estudarmoso método Kanban,vamos diferenciar a palavra Kanban com “K”maiúsculo eKanban com “k”minúsculo.
+O kanban refere-se àutilização de um quadro com o fluxo de trabalho, com os respectivos cartões querepresentamas
+atividades, sua origeméjaponesa esignifica literalmente “cartão” ou “sinalização”, e o Kanban refere-se ao método de
+desenvolvimento ágil criado em 2005, influenciado pelo Sistema Toyota de Produção.
+No  modelo  tradicional  corporativo,  a  transição  de  mudanças  muitas  vezes acontece de forma radical em um
+determinado período de tempo,porém como toda mudança,  existe  um  período  de  adaptação no  qual possíveis  prejuízos
+poderão acontecer, até tal mudança se estabilizar e trazer o retorno desejável. Dependendo do nível da mudança radical
+proposta pela organização, a mesma não possui estrutura suficiente para suportar a mudança e pode decidir voltar atrás,
+ou,no pior cenário,até mesmo  quebrar.  Esse  tipo  de  comportamento  organizacional,em  que determinada mudança  do
+status  quodentro  de  um  espaço  de  tempo  limitado    impactará  a capacidade organizacional,é conhecidocomo Kaikaku
+Para  trabalharmosmudanças  de  forma  incremental,  sem  que  o  período  de adaptação seja muito grandee diminuindo o
+risco da mudança, temos a abordagem de evolução Kaizen, que visa ciclos de iterações e melhorias contínuas referentes à
+mudança organizacional, com o objetivo de eliminar desperdícios e fazer “Hoje melhor do  que  ontem,  amanhã  melhor  do
+que  hoje!”
+Quando  falamos  sobre  metodologias  de  desenvolvimento  ágil,  o  modelo  de Evolução  Kaizen  é  extremamente
+aderente  e  está intrínseco  na  filosofia  das metodologias, e o método Kanban é um ótimo exemplo, ele é composto por
+um fluxo de valor no qual as etapas de trabalho são mapeadas em um quadro kanban, e as atividades andam da esquerda para
+a direita, e cada etapa de trabalho adiciona uma melhoria  de  valor  naquele  item,  e  ao  chegar àdireita  do  kanban
+esse  item  estará concluídoe entregando um valor completo
+
+sistema puxado x sistem empurrado
+O  Sistema  empurrado  faz  a  produção  ser  baseada  na  demanda,  onde  dada uma quantidade de trabalho X, esse
+trabalho é “empurrado” para uma equipe que muitas vezes nãotem a capacidade para executá-lo, exigindo 100% de ocupação
+do tempo para o desenvolvimento, para que um grande lote de trabalho seja entregue. Os principais efeitos de um sistema
+empurrado sãoa demora para entrega uma vez que  todos  estão  100%  ocupados,  e  isso  não  significa  100%  de
+produtividade, sobrecarga de trabalho, entregas em grandes lotes, e no pior dos cenários o burnout
+O  método Kanban  trabalha  com  o  conceito  de  sistema  puxado, em  que os profissionais “puxam” o trabalho a ser
+feito quando existe capacidade para executá-lo,  evitando  sobrecargas,uma  vez  que  a  capacidade  é  limitada  dentro
+de  limites estabelecidos corretamente, para assim prover um equilíbrio entre a capacidade do time e a demanda que o
+mesmo pode executar
+
+Princípios
+Comece com o que você tem hoje
+Concorde em propormudanças incrementais e evolucionárias
+Respeite o processo atual, papéis, responsabilidade e títulos
+Encoraje atos de liderança em todos os níveis
+
+Práticas
+Visualizar  o  fluxo de trabalho
+Limitar o trabalho em progresso
+Gerenciar  e  medir  o  fluxo
+Tornar as políticas de processo explícitas
+Implementar ciclos de feedback
+Melhore colaborativamente
+
+Elementos do sistema
+Quadro kanban
+Colunas   do   kanban
+Raias
+Cartões
+Limite  de  WIP
+Políticas  explícitas
+
+O método Kanban é recomendado para times que já conhecem a cultura ágil e precisam   trabalhar   em   escopos   de
+trabalho   variados   (manutenção,   novas funcionalidades,  operação).
+
+
+Extreme Programming
+Extreme Programmingou  XP  é  uma  metodologia de  desenvolvimento  de software leve e não prescritivo, criado em 1997,
+que procura levar ao extremo as boas práticas de engenharia de software, com foco em agilidade e qualidade,e assim como
+o Scrum e Kanban, se apoia em valores e práticas
+
+Valores
+Comunicação:O XP é organizado em práticas que não podem ocorrer sem a comunicação, como,por exemplo,a programação em
+pares, em que os desenvolvedores  estão juntos  programando  em  um  único  computador, trocando informações e se
+comunicando constantemente
+
+Simplicidade:Muitas  vezes  ao  iniciarmos  o  desenvolvimento  de  uma demanda, acabamos por fazer algo mais complexo
+do que o necessário, o XP preza  por fazer  o  mais  simples  possívelno  agorae,caso  seja necessário, algo mais
+complexo no amanhã
+
+Feedback:O  XP  procura  alcançar  o  feedback  da  forma  mais  rápida possível, através   de   testes   automatizados,
+conseguimos   respostas imediatas se aquilo que foi implementado ou alterado está funcionando
+
+Coragem: Para  trabalhar  com  o  XP,  coragem  é  necessária  para  dar  e receber feedback, fazer o que precisa ser
+feito, descartar o código ruim e protótipos criados que não devem virar produtos, aprender com os erros e acreditar na
+capacidade de reagir a mudanças
+
+Respeito: Os membros da equipe precisam respeitar uns aos outros e seus respectivos   pontos   de   vista,   saber
+ouvir   e   falar   contribuirá   com   a comunicação e com o sucesso do projeto
+
+Práticas
+Propriedade Coletiva(Collective  Ownership)
+Padronização     de Código(Coding     Standards)
+Versões  Pequenas  (Small  Releases)
+Planejando  o  jogo (Planning  Game)
+Testes de aceitação (Customer Tests)
+Programação Pareada (Pair Programming)
+Desenvolvimento orientado a testes (Test Driven Development)
+Integração   contínua (Continuous   Integration)
+Design  Simples (Simple  Design)
+Refatoração (Refactoring)
+Metáforas (Metaphor)
+Semana  de  40  horas (Sustainable  Pace)
+
+Recomendado  para  times  que  necessitam  disseminar  o  conhecimento,  o Extreme Programmingfoca muito mais em
+processos e práticas em volta do software do  que  nos  processos  de  trabalho  e  pessoas,  e  para  compensar  isso,
+é possível aplicar o XP com outras metodologias de desenvolvimento que o completam.O Extreme Programmingpropõe uma série
+de práticas que visam o melhor para o desenvolvimento de software, porém é comum não conseguir aplicar todas elas,
+o ideal é escolher as práticas mais viáveis dentro do possível do escopo de trabalho e aplicá-las,   principalmente   as
+práticas  relacionadas   aostestes   de   aceitação   e integraçãopara melhorarainda mais a qualidade do produto
+'''
