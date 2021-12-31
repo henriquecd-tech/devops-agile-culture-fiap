68c0ea2db0d0d4da97083747e9b13f435872cfec
Henrique Daniel <henriquec.daniel@hotmail.com>
2021-12-31 15:36:33 -0300

    dominios-de-conhecimento-da-agilidade-e-suas-metricas.py

---
+++ dominios-de-conhecimento-da-agilidade-e-suas-metricas.py
@@ -0,0 +1,146 @@
+'''
+A  agilidade  com  suas  metodologias,  práticas  e  culturas  são  cada  vez  mais utilizadas  por  times  de
+desenvolvimento  de  software,  mas  será  que  a  agilidade  se limita  apenas  aos  times  de  desenvolvimento? A
+resposta  é  não,  aagilidade tem influenciadocomo um todo o funcionamento dasempresas, impactando diretamente os
+domíniosde NEGÓCIO,CULTURA,ORGANIZAÇÃOeTÉCNICO,  conhecidos como DOMÍNIOS DE CONHECIMENTO DA AGILIDADE
+
+Na agilidade, as métricas fazem parte do diaadia e devem ser utilizadas como fonte de dados, diminuindo o espaço para
+o “achismo” ou até mesmo evitandoas escolhas que são tomadas “no grito”.Comas métricas, você ganha empoderamento para
+as tomadas de decisões com baseem dados e fatos
+
+Eficácia e eficiência são palavras muito semelhantes e porvezes são utilizadas como sinônimos, porém existe uma
+sutildiferença entre elas.Simplificando, eficácia está relacionada ao ato de “fazer as coisas certas”e eficiência em
+“fazer certo as coisas”;no cenário organizacional,a eficiência está mais ligada ao nível operacional e a eficácia,
+ao nível gerencial.
+A eficiência estaria ligada aodomínioorganizacional queserefereàforma de trabalho, eaeficácia ao negócio que está ligado
+aos objetivos e às metas daorganização
+
+Métricas de Eficiência (Organizacional)
+As métricas de eficiência são o melhor caminhopara sedeterminar seestamos trabalhando da melhor forma e identificar
+gargalos, solucionando-os em busca damelhoria continua
+
+Lead Time
+o LEAD TIME(tempo de espera)nos traz informações sobre o númerode dias (tempo) entre o inícioe  o  fim  do  processo
+de  entrega
+
+time to market
+é o tempo que um produto  leva desde  a  sua  concepçãoaté  que  ele  esteja  totalmente  disponível  para venda ou
+consumo
+
+Cycle TimeouLocal Lead Time
+O Lead Timeolha  para  o  tempo  total  do  processo  de  entrega,e  como normalmente busca-se diminuir esse tempo, é
+necessário identificar e analisar cada tempo específico  de  determinada  etapae,assim,realizar  alguma  ação,  para
+isso utilizamos o CYCLE TIME(tempo do ciclo), também chamado de LOCAL LEAD TIME(tempo de espera  local),que nos dáa
+informação entre  o inícioe o fim  de uma ou mais etapas do processo.
+
+Waiting Timee Touching Time
+Olhe para  o  kanban:temos  colunas  de  ação  (fazendo)  e  colunas  de espera(pronto,  backlog,  à  fazer,
+aguardando...). O tempo  que  o  item  fica  nas  colunas  de esperaé chamado de WAITING TIME(tempo de espera), e o
+tempo que o item fica na coluna de ação é chamado de TOUCHING TIME(tempo de ação).É comum  acreditar que quanto maior o
+Touching Timee menor o Waiting Time,maisutilização de recursos e um melhor desempenho, o que não é verdade.
+Estudosde Harvard comprovam que o tempo de espera dobra quando a taxa de ocupação vai de 60% para 80%, dobra novamente
+de 80% para 90% e dobra mais uma vez quando vai de 90% para 95%. Oideal é balancearmos entre espera e ação para melhorar
+o desempenho.
+
+Flow Efficiency
+Para sabermos se o nosso fluxo de trabalho está eficiente,utilizamos a métrica de FLOW EFFICIENCY(fluxo de eficiência).
+Para calculá-lo, basta dividir o Touching Time pela  soma  entre Waiting Time e Touching Time e depoi smultiplicar  por
+100. Vamos exemplificar:um item que demorou 10 dias para ser entregue, dos 10 dias, 7 dias ele ficou em espera e 3 em
+trabalho, logo a conta seria 3 / 10 * 100, e entãoa eficiência do fluxo para esse item que foi entregue foi de 30%.
+
+Por incrível que pareça, na maioria das vezes,as atividades ficam mais tempo em  espera  do  que  em  ação,  e  quando
+a  organização  não  se  atenta  para  isso,geralmente  seu  fluxo  de  eficiência fica em  torno  de  15%,  ou  seja,
+85%  do  ciclo  da atividade éesperando  algum  processo ou  alguma  coisa  acontecer.  Na  maioria  dos casos,podemos
+alcançar  uma  porcentagem  muito  melhor  no  fluxo  de  eficiência  e,consequentemente,uma maior redução de Lead Time,
+concentramos os esforços na resolução das causas raízes de tempo de espera
+
+Working In Progress(WIP)
+(trabalho  em  progresso) é  essencial  para sabermos  a  quantidade  de  itens  que  estão  sendo  trabalhados,  para
+assim  limitar  o WIP e controlar o ritmo da equipe e,consequentemente,melhorar o fluxo de eficiência, pois,em geral,
+quanto mais itens houver em andamento, maior é o tempo de entrega. O WIP é composto por itens em andamento, itens em
+espera e itens bloqueados
+
+Throughput
+O THROUGHPUT(vazão)é   a   quantidade   de   itens   finalizados   em   um determinado período de tempo, e geralmente
+buscamos o  maior Throughputcom o menor Lead Timepossível. A fórmula do cálculo do Throughputé a divisão entre a
+quantidade de itens finalizados e otempo, alguns exemplos de cálculode Throughputsão itens entregues por mês ou por
+semana,Story Pointspor Sprint, entre outros
+
+Cumulative Flow Diagram(CFD)
+No cenário das métricas, um diagrama muito famoso é o CUMULATIVE FLOW DIAGRAM(diagrama de fluxo cumulativo).Eleexibe o
+progresso de um fluxoem que há  diversos  estágios  pelos  quais  os  itens  de  trabalho  devem  passar  até  ficarem
+prontos, com  basenos  eixos  de  atividades versustempo,  oferecendo  uma  rápida visão geral do que está acontecendo
+no fluxo de trabalho
+
+Control Chart
+O  principal  objetivo  do CONTROL  CHART(gráfico  de  controle)é  detectar  o intervalo de variação do Lead Timedas
+atividades;por exemplo, se uma atividade for concluída em 3 dias, outra em 7 dias e outra em 30 dias, você pode mapear
+os Lead Timesem um ambiente visual para detectar anormalidades, com base no histórico das atividades
+
+
+
+Métricas de Eficácia (Negócio)
+É essencial emorganizações medir como andamos objetivos e as metas, para,se necessário,tomar alguma ação o  quanto antes
+Para isso temos as  métricas de eficácia.
+
+Return Of Investiment(ROI)
+Vários projetos podem ser iniciadosna empresaealgo que sempre é levantadoé qual será o RETURN OF INVESTIMENT(retorno
+sobreinvestimento), uma métricade  performance  financeiraque  expressa  a  relação  entre  o  valor  investido  em  um
+negócio e o valor obtido em retorno, utilizada para qualificar se um investimento vale a pena ou não financeiramente
+
+Net Promoter Score(NPS)
+O NET PROMOTER SCOREé uma metodologia de pesquisa de satisfação dos clientes,  sendo  considerado  um  dos  mais
+importantes  indicadores  de  desempenho, cujoobjetivo éavaliar  quão  recomendadaé  uma  empresa,  negócio  ou  serviço
+
+Churn
+O CHURNé  uma  métrica  que informaa  porcentagem  de clientesque abandonaramo negócio
+
+Métricas de Atmosfera (Cultural)
+Para  o  domínio cultural,  temos  as  métricas  de  atmosfera(ou  ambiente), não com  o  objetivo  de  medir  as
+pessoas,  mas  sim  a  satisfação  com  relação  a  diversos aspectos da organização, a fim detornar o ambiente cada vez
+mais saudável para otrabalho.
+
+Turnover
+O TURNOVER(rotatividade de pessoas)está relacionado com o desligamento de  alguns  funcionários
+(seja por  demissão  ou  pedido  de  demissão)e  a  entrada  de outros  para  substituição
+
+Happiness Radar
+O HAPPINESS  RADAR(radar  da  satisfação)contribuiparaa  pesquisa  de clima com o objetivo de identificar a satisfação
+do profissional com a organização, que pode incluir diferentes pontos de avaliação,como processos, ferramentas,
+entregas, time, empresa,entre outros.Vale identificar aquilo que é  mais importante,avaliar e colocar no
+Happiness Radar.
+
+
+Métricas de Qualidade (Técnica)
+Abordando  o  domínio  técnico,temos  as  métricas  de  qualidade,  que  estãodiretamente ligadasàqualidade do que foi
+produzido e entregue ao cliente, e pode impactar diretamente no sucesso da organização
+
+Notificação de Problemas
+A métrica  referente  ao  número  de bugs(falhas)  encontrados,  defeitos  ou devolução nos produtos e serviços, número
+de chamados ou reclamações devem ser acompanhadosde  perto  e  resolvidos  o  mais  rápido  possível. Talvez  não  seja
+perceptível,  mas  um  problema  pode  impactar  financeiramente  desde  um  simples telefonema para acentral de
+atendimento (que custa um valor para a empresa),a fim detirar  uma dúvidasobre um  mal funcionamento do software, até
+um problema  ou bug de vulnerabilidade de segurança que pode expor a organização e causar milhões em prejuízo financeiro
+, além de impactar negativamente a imagem no mercado
+
+Cobertura de Testes
+Para contribuir com a diminuição da métrica de notificação de problemas, temos a  métrica  da  porcentagem  de
+cobertura  de  testes  do  código.  A  cobertura  de  testes fornece uma visão rápida de algum trecho do software que
+foi feito e não está sendo coberto  pelos  testes, o que  além  de  aumentar  o  número  de  débitos  técnicos,  pode
+potencializar as chances de um comportamento desconhecido, bug ou vulnerabilidade de segurança.Investir no aumento da
+cobertura de testes é sempre bom, mas ter um número alto de cobertura não significaque o software está bem testado,
+sempre analise o que é  mais  importante ser  testado para  a  organização,  focando  nas  funcionalidades
+principais(core), automatizando sempre que possível e diminuindo a carga de testes manuais
+
+
+Métricas Tóxicase de Vaidade
+Quando  o  assunto émétricas,  temos  que  tomar  cuidado  com  aschamadasmétricas tóxicas, cujosobjetivos são medir
+os indivíduos ou comparar os times,o que,consequentemente,instiga  o  aumento  de  competição  não  saudável  e a
+diminuição da colaboração entre as pessoas.Um exemplo de métrica tóxica tradicional é a medição de tarefas entregues por
+determinada pessoa, quantidade de bugs corrigidos por outra pessoae a famosa foto do “Funcionário do Mês”.
+Tais  métricas acabam estimulando a  competitividade entre pessoas, que buscarão bater as metas e querer ser melhores
+que as outras, o que diminuirá drasticamente a colaboração, já que os demais são considerados meros concorrentes.
+Outro  erro  comum  é  recuperar  as  métricas  de  um  time  e  utilizá-las  para comparar  com  outro  time,  que  na
+maioria  das  vezes  possui  um  contexto  diferente. Cada time possui características específicas e,ao compará-los,
+acaba-sepor gerar novamente mais competição e menos colaboração
+'''
