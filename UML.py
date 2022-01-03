d2561e6094622d896ac12c27d33404a017334715
Henrique Daniel <henriquec.daniel@hotmail.com>
2022-01-02 23:41:29 -0300

    UML.py

---
+++ UML.py
@@ -0,0 +1,56 @@
+'''
+O desenvolvimento de software é uma atividade bastantecomplexa, e em razão disso,existemdificuldades e surgemvários
+problemas  quando  não  há  um  padrão comum   de   interpretação   paraaequipe   envolvida.   Para   tanto,
+é   necessário desenvolver o software sem que hajaum retrabalho, cujamanutenção no futuro seja fácil e no qualtodos os
+envolvidos interpretem amesmalinguagem. A UML propõe a construção de diagramaspara o desenvolvimento de sistemas
+orientados a objetos a fim   defacilitar,   por   meiode   notação visual,uma   representação   comum emdeterminado
+momento do desenvolvimento de software
+
+AUML –Unified Modeling Languageou Linguagem de Modelagem Unificada –é uma linguagem visual utilizada para modelar
+softwares baseados no paradigma da orientação objetos. É uma linguagem de modelagem de propósito geral que pode ser
+aplicada a todos os domínios da aplicação. Estalinguagem tornou-se,nos últimos anos, a linguagem padrão de modelagem
+adotada internacionalmente pela indústria de engenharia de software
+
+...A análise foi concebida na década de 1970esurgiu com o objetivo de construir sistemas  mais  eficientes  e  eficazes,
+por  meiodo  uso  de  modelos  e  padrões estruturados para facilitar o entendimento da necessidade do usuário,
+pois antes do uso  da  análise,  ossistemas  não  atendiam  adequadamente  a  real  necessidade  dosusuários,
+além dosprojetos queextrapolavam os prazose os orçamentospor falta de entendimento da necessidade do usuário, o “ouvir”
+e programar sem estudo prévio causava  grandes  problemas  no  desenvolvimento  de sistemas.Portanto,antes  da análise
+não    haviapadrões,    ferramentas,    técnicas nemmétodos    para    o desenvolvimento de sistemas
+
+Orápido crescimento da capacidade computacional das máquinas resultou na demanda por sistemas de software cada vez mais
+complexos. Portanto,o surgimento destes sistemas complexos resultou na necessidade de reavaliação das formas de
+desenvolvimento de sistema, consequentemente,o surgimento de metodologias, práticas, técnicas e padrões para a modelagem
+de sistema
+
+Histórico UML
+Entre 1991 e 1995,começouo desenvolvimento de técnicas para modelagem de sistemas orientados a objetos, sendo os
+principais contribuintes:Ivair Jacobson, James   Rumbaugh   e   Grady   Booch, os   quaisreaproveitaram as   melhores
+características  e  artefatos  das  técnicas  propostas(conforme  FiguraHistórico  da orientação  de  objetos  até  o
+surgimento  da  UML)  para  o  desenvolvimento  da  UML. Inicialmente era chamada de UM (Unified Method), somente com a
+entrada de Booch,quando os três trabalharam na empresa Rational Software,as três propostas foram unificadas esurgiu
+realmente a UML (Unified Modeling Language).Em 1997, a OMG (Object Management Group),uma organização que define e
+ratifica apadronização de assuntos relacionados àorientação de objetos, aprovou e adotou o padrão UML para a modelagem
+de sistemas orientados a objetos,edesde então,esse  padrão tem  grande  aceitação  no  mercado  e na  comunidade  de
+desenvolvedores.A  UML  (Unified  Modeling  Language) se  tornou  uma  linguagem  visual  ou notação visual que permite
+representar os paradigmas da orientação a objetos para modelagem  de  sistemas, por  meioda  notação  visual  com  os
+diagramas é  possível representarvárias perspectivas do sistema.
+
+UML é uma linguagem visual para construir, documentar e especificar sistemas orientados a objetos. Não confunda com
+linguagem de programação, ela não tem relação com o processo de desenvolvimento e pode ser definida como um padrão de
+notação visual.Responsável por toda documentação da arquitetura do sistema, o UML especifica os requisitos e auxilia
+nos casos de testes, oferecendo suporte no planejamento e gerenciamento de cada versão do software.Não confunda UML
+com: linguagem de programação, método ou metodologia!
+
+Quaissão as razões para construção de modelosvisuais?
+Facilita a comunicação entre as pessoas envolvidasno projeto de software.
+Redução do tempo e do custo de desenvolvimento.
+Futura manutençãodo sistema.
+Complexidade do sistema.
+Controle da complexidade.
+Gerenciamento dos entregáveis.
+Gerenciamento de mudanças.
+Reduz asambiguidades.
+
+
+'''
